b4ae3d468631a4f9c9cb00942f2031c35acc250e
Blake Nall <blake@proton.me>
2024-05-08 13:37:02 -0500

    Updated database settings

--- arkcomputing/settings.py
+++ arkcomputing/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = 'arkcomputing.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'arkcomputing': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test-db',
-        'USER': 'blake',
+        'NAME': 'arkcomputing',
+        'USER': 'arkcomputing',
         'PASSWORD': os.environ.get("DB_PASS"),
-    }
+    },
 }
 
 
