f3e8f2cee00b4a822467023ddf6348fb7a8c9453
Blake Nall <blakenall@proton.me>
2024-05-08 19:11:59 -0500

    Reorganized imports

--- ecom/admin.py
+++ ecom/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from ecom.models import ArkComputingBuild, ArkComputingPart, ArkComputingUser, ArkComputingCustomBuild
+from ecom.models import ArkComputingPart
+from ecom.models import ArkComputingBuild
+from ecom.models import ArkComputingUser
+from ecom.models import ArkComputingCustomBuild
 
 admin.site.register(ArkComputingPart)
 admin.site.register(ArkComputingBuild)
