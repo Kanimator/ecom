0fe2a5661aa6556747ae04e81ac584c8e7807f3d
Blake Nall <blakenall@proton.me>
2024-05-08 19:10:13 -0500

    Added new models to admin site

--- ecom/admin.py
+++ ecom/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from ecom.models import ArkComputingBuild, ArkComputingPart
+from ecom.models import ArkComputingBuild, ArkComputingPart, ArkComputingUser, ArkComputingCustomBuild
 
 admin.site.register(ArkComputingPart)
 admin.site.register(ArkComputingBuild)
+admin.site.register(ArkComputingUser)
+admin.site.register(ArkComputingCustomBuild)
