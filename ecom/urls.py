78964aada879d67b4968dd27ba0d8a9c74bdacf5
Blake Nall <blakenall@proton.me>
2024-06-14 16:07:34 -0500

    Updated name

--- ecom/urls.py
+++ ecom/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:product_id>/", views.get_product, name="product"),
+    path("<int:product_id>/", views.get_product, name="get product"),
 ]
