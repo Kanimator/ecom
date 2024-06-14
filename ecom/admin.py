9d6fd55f9a57f59d2f871853ddb7d3593610963b
Blake Nall <blakenall@proton.me>
2024-06-14 15:58:15 -0500

    Sorted admin.py

--- ecom/admin.py
+++ ecom/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from ecom.models import Cart, Category, Order, Product
 
-admin.site.register(Product)
-admin.site.register(Category)
 admin.site.register(Cart)
+admin.site.register(Category)
 admin.site.register(Order)
+admin.site.register(Product)
