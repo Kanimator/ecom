a07253166d607f0fd7e0fe6dad443843844b9b1b
Blake Nall <blakenall@proton.me>
2024-06-20 16:01:39 -0500

    Updated dir name

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 git clone https://github.com/crimsonslate/ecom.git
-cd arkcomputing-site/
+cd ecom/
 python -m venv .venv && source .venv/bin/activate
 pip install -r requirements.txt
 python manage.py makemigrations && python manage.py migrate
