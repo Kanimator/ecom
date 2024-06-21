3dc2b1739c8017616fd2301cc9105caf08090a77
darthnall <106842611+darthnall@users.noreply.github.com>
2024-06-21 11:56:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-git clone https://github.com/crimsonslate/ecom.git
-cd ecom/
 python -m venv .venv && source .venv/bin/activate
 pip install -r requirements.txt
 python manage.py makemigrations && python manage.py migrate
