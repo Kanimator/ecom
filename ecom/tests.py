222f25c0771b805f5ea8d762c43535ce70dd88f3
Blake Nall (darthnall) <blakenall@proton.me>
2024-07-06 19:56:23 -0500

    Removed old tests

--- ecom/tests.py
+++ ecom/tests.py
@@ -1,43 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from ecom.models import Cart, Product, SquareToken
-
-
-class SquareTokenModelTests(TestCase):
-    def test_get_and_set_access_token(self):
-        """Succeeds if an arbitrary access token can be saved and retrieved."""
-        unencrypted_access_token = "super_secure_access_token"
-        test_user = User.objects.create_user(
-            username="test_user",
-            email="test_user@domain.com",
-            password="test_password",
-        )
-        test_token = SquareToken.objects.create(user=test_user)
-
-        test_token.access_token = unencrypted_access_token
-
-        # Fails if the access token was incorrectly encrypted
-        self.assertNotEqual(test_token._access_token, unencrypted_access_token)
-        # Fails if the access token was incorrectly decrypted
-        self.assertEqual(test_token.access_token, unencrypted_access_token)
-
-    def test_get_and_set_refresh_token(self):
-        """Succeeds if an arbitrary refresh token can be saved and retrieved."""
-        unencrypted_refresh_token = "super_secure_refresh_token"
-        test_user = User.objects.create_user(
-            username="test_user",
-            email="test_user@domain.com",
-            password="test_password",
-        )
-        test_token = SquareToken.objects.create(user=test_user)
-
-        test_token.refresh_token = unencrypted_refresh_token
-
-        # Fails if the refresh token was incorrectly encrypted
-        self.assertNotEqual(test_token._refresh_token, unencrypted_refresh_token)
-        # Fails if the refresh token was incorrectly decrypted
-        self.assertEqual(test_token.refresh_token, unencrypted_refresh_token)
+from ecom.models import Cart, Product
 
 
 class CartModelTests(TestCase):
