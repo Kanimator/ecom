b3ea2e579f854c31343bcfd14a1434bc536bcacc
Blake Nall (darthnall) <blakenall@proton.me>
2024-07-06 19:52:47 -0500

    Added cart views

--- ecom/views/cart.py
+++ ecom/views/cart.py
@@ -5,40 +5,16 @@ from django.views import View
 from ecom.models.cart import Cart
 from ecom.models.product import Product
 
+from typing import Any
 
 class CartView(View):
     template_name = "ecom/cart.html"
+    partial_template_name = "ecom/partials/cart.html"
+
+    def get_context_data(self, request: HttpRequest, *args, **kwargs) -> Any:
+        context = super().get_context_data(*args, **kwargs)
+        context["cart"] = Cart.objects.get_or_create(user=request.user)
+        return context
 
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        cart, _ = Cart.objects.get_or_create(user=request.user)
-        context = {"title": "Cart", "cart": cart}
-        return render(request, self.template_name, context=context)
-
-    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        context = {"title": "Shop"}
-        if request.htmx:
-            return render(request, self.template_name, context=context)
-        else:
-            return render(request, self.template_name, context=context)
-
-
-def clear_cart(request: HttpRequest) -> HttpResponse:
-    cart, _ = Cart.objects.get_or_create(user=request.user)
-    cart.clear_items()
-    cart.save()
-    context = {
-        "title": "Cart",
-        "cart": cart,
-    }
-    return render(request, "ecom/cart.html", context=context)
-
-
-def add_to_cart(request: HttpRequest, product_id: int, quantity: int) -> HttpResponse:
-    product = get_object_or_404(Product, id=product_id)
-    cart, _ = Cart.objects.get_or_create(user=request.user)
-    cart.add_product(product=product, quantity=quantity).save()
-    context = {
-        "title": "Cart",
-        "cart": cart,
-    }
-    return render(request, "ecom/cart.html", context=context)
+        return render(request, self.template_name)
