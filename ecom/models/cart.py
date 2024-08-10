6681a4d767ff34bdf090051247d0f13fbca0c76f
Blake Nall (darthnall) <dev@darthnall.com>
2024-08-09 19:39:36 -0500

    Simplified cart logic

--- ecom/models/cart.py
+++ ecom/models/cart.py
@@ -1,14 +1,23 @@
 from django.db import models, transaction
+from django.contrib.auth.models import User
 
 from ecom.models.product import Product
 
+class CartItem(models.Model):
+    """Intermediate model to represent a product in a :model:`ecom.Cart`."""
+
+    cart = models.ForeignKey("Cart", on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self) -> str:
+        return f"{self.quantity} of '{self.product.name}'"
 
 class Cart(models.Model):
-    """Holds :model:`ecom.Product`s that a user can purchase all at once."""
+    """Holds :model:`ecom.CartItem`s that a user can purchase all at once."""
 
-    user = models.OneToOneField("auth.User", on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
-    products = models.ManyToManyField("Product", through="CartItem")
 
     def __str__(self) -> str:
         return f"{self.user.username}'s cart"
@@ -19,17 +28,9 @@ class Cart(models.Model):
         except Product.DoesNotExist:
             raise ValueError(f"No product with {product_id = } exists.")
 
-    def get_cartitem_by_product(self, product: Product) -> "CartItem":
-        try:
-            return CartItem.objects.get(cart=self, product=product)
-        except CartItem.DoesNotExist:
-            raise ValueError(f"No product with id `{product.id}` present in this cart.")
-
     @transaction.atomic
     def create_order(self) -> None:
         """Creates an :model:`ecom.Order` based on this cart's items list."""
-        if not self.items.exists():
-            raise ValueError("Cannot create order without items.")
 
         return None
 
@@ -37,7 +38,7 @@ class Cart(models.Model):
     def clear_items(self) -> None:
         """Removes all instances of :model:`ecom.CartItem` from this cart."""
         self.items.all().delete()
-
+        self.save()
         return None
 
     @transaction.atomic
@@ -47,14 +48,13 @@ class Cart(models.Model):
             raise ValueError("Quantity must be a positive integer.")
 
         product = self.get_product_by_id(product_id)
-        cartitem, created = CartItem.objects.get_or_create(cart=self, product=product)
+        cart_item, created = CartItem.objects.get_or_create(product=product)
         if created:
-            cartitem.quantity = quantity
-            cartitem.save()
+            cart_item.quantity = quantity
         else:
-            cartitem.quantity += quantity
-            cartitem.save()
+            cart_item.quantity += quantity
 
+        cart_item.save()
         return None
 
     @transaction.atomic
@@ -64,27 +64,19 @@ class Cart(models.Model):
             raise ValueError("Quantity must be a positive integer.")
 
         product = self.get_product_by_id(product_id)
-        cartitem = self.get_cartitem_by_product(product=product)
+        try:
+            cart_item = self.items.get(product=product)
+        except CartItem.DoesNotExist:
+            raise ValueError(f"Product <{product.id}:'{product.name}'> not found in this cart.")
 
-        if quantity > cartitem.quantity:
+        if quantity > cart_item.quantity:
             raise ValueError(
-                f"Invalid quantity '{quantity}'. Quantity cannot be greater than {cartitem.quantity}."
+                f"Invalid quantity '{quantity}'. Quantity cannot be greater than {cart_item.quantity}."
             )
-        elif quantity == cartitem.quantity:
-            cartitem.delete()
+        elif quantity == cart_item.quantity:
+            cart_item.delete()
         else:
-            cartitem.quantity -= quantity
-            cartitem.save()
+            cart_item.quantity -= quantity
+            cart_item.save()
 
         return None
-
-
-class CartItem(models.Model):
-    """Intermediate model to represent a product in a :model:`ecom.Cart`."""
-
-    cart = models.ForeignKey("Cart", related_name="items", on_delete=models.CASCADE)
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-
-    def __str__(self) -> str:
-        return f"{self.quantity} of '{self.product.name}'"
