e67203a2b50400e5dda9ba9754dc289a5e0ce97f
Blake Nall (darthnall) <dev@darthnall.com>
2024-08-14 21:06:55 -0500

    Made users actually optional

--- ecom/models/cart.py
+++ ecom/models/cart.py
@@ -16,7 +16,7 @@ class CartItem(models.Model):
 class Cart(models.Model):
     """Holds :model:`ecom.CartItem`s. User is optional."""
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True, default=None)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
